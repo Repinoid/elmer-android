5c465e32bde932e8e3c82e710239e3f7c5d1e34e
Repinoid <tazet@narod.ru>
2026-05-26 09:13:11 +0400

    fix: fixed debug keystore for repeatable installs + version 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.elmer.client"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0-test"
+        versionCode = 3
+        versionName = "0.3.0-test"
     }
 
     buildTypes {
