6ba9861e234af83730bddc8f4fdaeeca654ea627
Repinoid <tazet@narod.ru>
2026-06-28 18:42:55 +0400

    fix: новое имя ELM Relay v2, v0.2.2-dev

--- raw/build.gradle.kts
+++ raw/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.elmer.raw"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.2.1-dev"
+        versionCode = 6
+        versionName = "0.2.2-dev"
     }
 
     buildTypes {
